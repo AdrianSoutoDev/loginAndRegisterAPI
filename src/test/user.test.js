d1024fbb3aa9b1f267c7fc2a1f16e63c650c6a57
Adrian Souto <adrian.souto@udc.es>
2022-01-20 04:37:33 +0100

    unit User tests with mock db

--- src/test/user.test.js
+++ src/test/user.test.js
@@ -6,50 +6,51 @@ const User = require('../models/user')
 const api = supertest(app)
 
 const { usernameExists, emailExists, findUserById} = require('../services/user_service');
-
-const newUser = ( async () => {
-    let user = new User({username: "adrian.souto", password: "somePassword", email: "adrian.souto@email.com", roles: []})
-    return await user.save()
-})
-
 const notExistingID = () => '000000000000000000000000';
+const existingID = () =>    '111111111111111111111111';
 
-afterEach( async () => {
-    await User.deleteMany({})
+User.findOne = jest.fn()
+User.findById = jest.fn()
+
+beforeEach(() => {
+    User.findOne.mockReset()
+    User.findById.mockReset()
 })
 
 test('username not exist', async () => {
+    User.findOne.mockReturnValueOnce(null)
     expect(await usernameExists("notUsername")).toBe(false)
 })
 
 test('username exist', async () => {
-    await newUser()
+    User.findOne.mockReturnValueOnce({id: existingID(), username: "adrian.souto", password: "somePassword", email: "adrian.souto@email.com", roles: []})
     expect(await usernameExists("adrian.souto")).toBe(true)
 })
 
 test('email not exist', async () => {
+    User.findOne.mockReturnValueOnce(null)
     expect(await emailExists("notEmail")).toBe(false)
 })
 
 test('email exist', async () => {
-    await newUser()
+    User.findOne.mockReturnValueOnce({id: existingID(), username: "adrian.souto", password: "somePassword", email: "adrian.souto@email.com", roles: []})
     expect(await emailExists("adrian.souto@email.com")).toBe(true)
 })
 
 test('find user by id not found', async () => {
-    await newUser()
+    User.findById.mockReturnValueOnce(null)
     id = notExistingID()
     expect(await findUserById(id)).toBe(null)
 })
 
 test('find user by id', async () => {
-    let user = await newUser()
-    let userId = user.id
-    let expected = (await findUserById(userId)).toJSON()
+    user = new User( {id: existingID(), username: "adrian.souto", password: "somePassword", email: "adrian.souto@email.com", roles: []} )
+    User.findById.mockReturnValueOnce(user)
+    let expected = (await findUserById(user.id)).toJSON()
     expect(expected).toStrictEqual(user.toJSON())
 })
 
 afterAll(async () => {
-    mongoose.disconnect()
+    await mongoose.disconnect()
     server.close()
 })
