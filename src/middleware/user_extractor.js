51232cdc4613016bfb3460dbb375d9e4a2433fa8
Adrian Souto <adrian.souto@udc.es>
2022-01-21 23:49:18 +0100

    fix when try to extract jwt without Authorization header

--- src/middleware/user_extractor.js
+++ src/middleware/user_extractor.js
@@ -3,6 +3,7 @@ const { JWTSECRET } = require('../../config.js')
 module.exports = (request, response, next) => {
   const auth = request.get('Authorization')
   let token = null
+
   if (auth && auth.toLowerCase().startsWith('bearer')) {
     token = auth.substring(7)
     const decodedToken = jwt.verify(token, JWTSECRET)
@@ -13,6 +14,8 @@ module.exports = (request, response, next) => {
 
     const { id } = decodedToken
     request.userId = id
+  } else {
+    return response.status(401).json({ error: 'Token missing or invalid' })
   }
 
   next()
